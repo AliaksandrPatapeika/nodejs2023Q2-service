fa8679deeb242d2cb24996a227f422499ae1ed9b
Aliaksandr Patapeika <aliaksandr.patapeika@wolterskluwer.com>
2023-08-19 12:57:59 +0400

    feat: add log file names according to assignment to constants

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -23,6 +23,16 @@ export const JWT_CONSTANTS = {
   TOKEN_REFRESH_EXPIRE_TIME: 'TOKEN_REFRESH_EXPIRE_TIME',
 };
 
+/**
+ * Log file names used in the application.
+ */
+export const LOG_FILE_NAMES = {
+  ERROR: 'error.log',
+  HTTP: 'http.log',
+  UNCAUGHT_EXCEPTION: 'uncaught-exception.log',
+  UNHANDLED_REJECTION: 'unhandled-rejection.log',
+};
+
 export const uuidVersion = '4';
 export const LOGIN_FIELD = 'login';
 export const LOGGING_LEVELS = ['debug', 'error', 'log', 'verbose', 'warn'];
